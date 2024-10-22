3d28bde4825effe8706a517e07ba4f52e1a722f4
Jonathan Martinez H <jonathan.martinez@bigwavedata.mx>
2024-10-22 16:19:02 -0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "APP_004_TIC_TAC_TOE"
+include(":app")
